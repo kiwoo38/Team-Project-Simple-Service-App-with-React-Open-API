b0542048df7c69fb4ea0c307549161c90deccea0
김기우 <kimki41233@gmail.com>
2025-10-17 04:41:14 +0900

    Add Kakao API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TasteLinkPage from './TasteLinkPage'; // ⚠️ 여기 슬래시는 앞에 점 꼭 붙이기! (‘./’)
+console.log("ENV KEY =", process.env.REACT_APP_KAKAO_APPKEY);
 
 function App() {
   return (
